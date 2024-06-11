a66e4e850ccfcd4ac7df921b556dafb2fda93963
JonasSkolnik <jonas.skolnik@student.uva.nl>
2024-06-11 12:36:40 +0200

    week 2 1.2

--- Lab_week2/lab2_part1/lab2_led.py
+++ Lab_week2/lab2_part1/lab2_led.py
@@ -8,12 +8,14 @@ from bleak import BleakClient
 from bleak import BleakScanner
 from bleak import discover
 
-ARDUINO_LOCAL_NAME = "BLE-AR41"  #Use the correct Arduino number in this identifier!!
+ARDUINO_LOCAL_NAME = "BLE-LAB41"  #Use the correct Arduino number in this identifier!!
 
-LED_UUID = "19b10001-e8f2-537e-4f6c-d104768a1214"
+LED_UUID = "19B10000-E8F2-537E-4F6C-D104768A1214"
+CHARACTERISTIC_UUID = "19B10001-E8F2-537E-4F6C-D104768A1214"
 
 on_value = bytearray([0x01])
 off_value = bytearray([0x00])
+
 
 async def find_ble_device(args: argparse.Namespace):
     print("scanning for 5 seconds, please wait...")
@@ -21,27 +23,27 @@ async def find_ble_device(args: argparse.Namespace):
     devices = await BleakScanner.discover(
         return_adv=True, cb=dict(use_bdaddr=args.macos_use_bdaddr)
     )
+    
+    # find your Arduino and return device and address
+    for d, a in devices.values():
+        if a.local_name == ARDUINO_LOCAL_NAME:
+            return (d, a)
 
-#
-# find your Arduino and return device and address
-    return (d,a)
 
-
-async def runmain(d,a):
-
+async def runmain(d, a):
     async with BleakClient(d.address) as client:
-        await client.connect()
-        print("Connected to Arduino")
-            
         svcs = await client.get_services()
-        print(f"Services: {svcs}")
+        # Get the characteristic object using its UUID
+        characteristic = svcs.get_characteristic(CHARACTERISTIC_UUID)
+        await client.write_gatt_char(characteristic, on_value)
+        await asyncio.sleep(1)
+        await client.write_gatt_char(characteristic, off_value)
 
-
+       
 # your code here to control the led.
 # use await statements and the bleak read_gatt_char and write_gatt_char
 # functions of Bleak
 #flash the LED 10 times with one second in between using an asynchronous sleep
-
 
 
 if __name__ == "__main__":
@@ -53,10 +55,9 @@ if __name__ == "__main__":
         help="when true use Bluetooth address instead of UUID on macOS",
     )
 
-
     args = parser.parse_args()
-    (d,a) = asyncio.run(find_ble_device(args))
-    if (d,a) != (None, None):
-      asyncio.run(runmain(d,a))
+    (d, a) = asyncio.run(find_ble_device(args))
+    if (d, a) != (None, None):
+        asyncio.run(runmain(d, a))
     else:
         print("arduino not found")
